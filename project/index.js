c9a7f4a1bd6ee68112114a147334f3394b1d37e8
IsaT <talantbekisa778@gmail.com>
2024-11-23 17:07:28 +0600

    update-1.6

--- project/index.js
+++ project/index.js
@@ -4,14 +4,17 @@ import fetchData from './fetch.js'
 import alterData from './alter.js'
 import updateData from './update.js'
 import deleteData from './delete.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 const { Pool } = pkg
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'usersbase',
-    password: '260909isa',
-    port: 5432,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
